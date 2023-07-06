e21a023860b5483fc21aa3991e72d4c44d61323d
P.Sang Le <105464800+SangTheInnovator@users.noreply.github.com>
2023-07-06 15:44:31 +0300

    Update ecr.py

--- ecr.py
+++ ecr.py
@@ -2,8 +2,6 @@ import boto3
 
 ecr_client = boto3.client('ecr')
 
-kms = boto3.client('kms', region_name='us-west-2')
-
 repository_name = "cloud-native-app-repo"
 response = ecr_client.create_repository(repositoryName=repository_name)
 
